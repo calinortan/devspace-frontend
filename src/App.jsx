1d0f5878f36053fd59ed18b2784b2c90510f5728
calinortan <calinortan@gmail.com>
2017-02-04 14:46:58 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Header from './components/header/Header.jsx'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className='Header'>
+        <div className='Header-Text Header-Block'>
+          Devspace
+        </div>
+        <div className='Header-Block'>
+            
+        </div>
+        <div className='Header-Icon'>
+
+        </div>
+        <div className='Header-Block Header-User'>
+            
+        </div>
+      </div>
+    );
+  }
+}
+export default Header;
