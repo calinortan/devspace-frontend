c2686f2329b5f7c0aef8cc0dcd7337e312d2385b
calinortan <calinortan@gmail.com>
2017-07-11 21:43:54 +0300

    added dummy chat component with styling

--- src/components/chatPanel/components/Conversation.jsx
+++ src/components/chatPanel/components/Conversation.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Chip from 'material-ui/Chip';
-import { green400, grey300, green100 } from 'material-ui/styles/colors';
+import { CardHeader } from 'material-ui/Card';
+import DevspaceAvatar from '../../../common/DevspaceAvatar.jsx';
+import TextField from 'material-ui/TextField';
+import { green400, grey300, grey500, grey50 } from 'material-ui/styles/colors';
 
 const messages = [
   {
@@ -11,6 +14,14 @@ const messages = [
   {
     text: 'I can t give you a better response ',
     userId: '321'
+  },
+  {
+    text: 'cool ',
+    userId: '321'
+  },
+  {
+    text: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ',
+    userId: '123'
   }
 ]
 class Conversation extends Component {
@@ -22,12 +33,18 @@ class Conversation extends Component {
   }
   getMessageChip(text, isOwnMessage) {
     const chipProps = {
-      backgroundColor: green400,
-      labelColor: grey300,
-      style: {
+      backgroundColor: isOwnMessage ? green400 : grey500,
+      labelColor: isOwnMessage ? grey300 : grey50,
+      labelStyle: {
         whiteSpace: 'initial',
         maxWidth: '200px',
-        display: 'block'
+        display: 'block',
+        padding: 0,
+        lineHeight: '20px'
+      },
+      style: {
+        padding: '7px',
+        boxSize: 'border-box'
       }
     }
     return <Chip {...chipProps}>
@@ -49,15 +66,34 @@ class Conversation extends Component {
       </div>
     });
   }
+  renderTextInput() {
+    return <TextField
+      hintText="Type in your message"
+      multiLine={true}
+    />;
+  }
   render() {
+
     return <section className={'Conversation'}>
-      {this.renderMessagesList()}
+      <CardHeader
+        title="Alexandra Ulici"
+        subtitle="Accenture"
+        avatar={<DevspaceAvatar src={'https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAO-AAAAJGQwM2VkN2I5LTY5ZDMtNDlkZC05MDNjLWFiOGE0ZWFjYzYzNg.jpg'} size={35} style={{top:'5.5px'}} userName='Alexandra Ulici'/>}
+        style={{backgroundColor:'#32b38c', padding:'6px'}}
+        textStyle={{paddingLeft: '10px'}}
+      />
+      <div className={'Conversation-Messages'}>
+        {this.renderMessagesList()}
+      </div>
+      <div className={'Conversation-Input'}>
+        {this.renderTextInput()}
+      </div>
     </section>
   }
 }
 
 Conversation.propTypes = {
   currentUserId: PropTypes.string,
-  otherUserId: PropTypes.string,
+  otherUserId: PropTypes.string
 }
 export default Conversation;
